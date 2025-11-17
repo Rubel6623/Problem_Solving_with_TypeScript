5ee1097c51be4e1119a96f472cc1dd29d19c5822
Rubel Rudra <rubelrudra27@gmail.com>
2025-11-17 10:59:00 +0600

    Create Person class to solve problem - 03

--- solution.ts
+++ solution.ts
@@ -18,3 +18,19 @@ const getLength = (input : string | number[] | string[]) => {
   }
 }
 
+
+
+class Person {
+  name : string;
+  age : number;
+  constructor(name : string, age : number){
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails():string{
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+
